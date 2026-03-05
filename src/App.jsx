e5dbe16bf72b055cabddb384003fa326763ac59c
vikaskumarram <jaivikash609@gmail.com>
2026-03-05 16:32:24 +0530

    edite landing page

--- src/App.jsx
+++ src/App.jsx
@@ -7,29 +7,32 @@ import { Services } from "./services";
 import { About } from "./About";
 import { Footer } from "./Footer";
 import { LandingPage } from "./LandingPage";
+// import AppBar from "@mui/material/AppBar";
 
 export default function App() {
   return (
     <>
-    
-      <nav className="navbar">
-        <img className="first-logo" src="public\V-taxi logo2.JPG" alt="" />
-        <Link to="Home">Home</Link>
-        <Link to="About">About</Link>
-        <Link to="Services">Services</Link>
-        <Link to="Contact"> Contact </Link>
-      </nav>
-    
-      {/* <BrowserRouter> */}
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="Home" element={<Home />} />
-        <Route path="About" element={<About />} />
-        <Route path="Services" element={<Services />} />
-        <Route path="Contact" element={<Contact />} />
-      </Routes>
-      <Footer />
-      {/* </BrowserRouter> */}
+      {/* <AppBar position="sticky"> */}
+        <nav className="navbar">
+          <img className="first-logo" src="public\V-taxi logo2.JPG" alt="" />
+          <Link to="Home">Home</Link>
+          <Link to="About">About</Link>
+          <Link to="Services">Services</Link>
+          <Link to="Contact"> Contact </Link>
+          
+        </nav>
+
+        {/* <BrowserRouter> */}
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="Home" element={<Home />} />
+          <Route path="About" element={<About />} />
+          <Route path="Services" element={<Services />} />
+          <Route path="Contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+        {/* </BrowserRouter> */}
+      {/* </AppBar> */}
     </>
   );
 }
