e8f81d70c1c56f585eb1b4184646c9325a1bfc75
vikaskumarram <jaivikash609@gmail.com>
2026-03-10 16:39:05 +0530

    landing page edite

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -1,7 +1,6 @@
 export function LandingPage() {
   return (
     <div className="landing-page">
-      <img src="" alt="" />
       <h2 className="landing-taxt">
         your destination our identity travel with Vikas-taxi
       </h2>
